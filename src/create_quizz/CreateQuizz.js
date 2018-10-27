303cc179e26c50f95bfb088a4f389c56ef5e2a57
Willian Carvalho <willian.carvalho@castlabs.com>
2018-10-27 13:55:58 +0200

    Added auto enable button

--- src/create_quizz/CreateQuizz.js
+++ src/create_quizz/CreateQuizz.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OuterLayer from '../outer_layer/OuterLayer';
 import MdlCell from '../outer_layer/MdlCell';
 import UpperMenu from '../upper_menu/UpperMenu';
+import {Link} from 'react-router-dom';
 
 export default class CreateQuizz extends React.Component {
   constructor(props) {
@@ -12,6 +13,12 @@ export default class CreateQuizz extends React.Component {
       quizzDescription: '',
       questions: []
     };
+  }
+
+  shouldButtonEnable() {
+    let name = this.state.quizzName;
+    let descr = this.state.quizzDescription;
+    return !(name.trim() && descr.trim());
   }
 
   handleChange(ev) {
@@ -39,25 +46,33 @@ export default class CreateQuizz extends React.Component {
             <h1>New Quizz</h1>
 
             <form action="#">
-              <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input className="mdl-textfield__input" type="text" id="sample3"/>
-                <label className="mdl-textfield__label" htmlFor="sample3">Text...</label>
+            <div className="mdl-grid">
+                <MdlCell cellLength="12">
+                <div className="mdl-textfield mdl-js-textfield">
+                  <label className="mdl-textfield__label" htmlFor="name">Quizz name</label>
+                  <input className="mdl-textfield__input" type="text" id="name" value={name} onChange={this.handleChange.bind(this)}/>
+                </div>
+                </MdlCell>
               </div>
-            </form>
-
-            <form action="#">
-              <div className="mdl-textfield mdl-js-textfield">
-                <label className="mdl-textfield__label" htmlFor="name">Quizz name</label>
-                <input className="mdl-textfield__input" type="text" id="name" value={name} onChange={this.handleChange.bind(this)}/>
-              </div>
-
-              <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <textarea className="mdl-textfield__input" type="text" rows= "1" id="descr" value={descr} onChange={this.handleChangeDescr.bind(this)}></textarea>
-                <label className="mdl-textfield__label" htmlFor="descr">Description</label>
+              <div className="mdl-grid">
+                <MdlCell cellLength="12">
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <textarea className="mdl-textfield__input" type="text" rows="5" id="descr" value={descr} onChange={this.handleChangeDescr.bind(this)}></textarea>
+                  <label className="mdl-textfield__label" htmlFor="descr">Description</label>
+                </div>
+                </MdlCell>
               </div>
             </form>
           </MdlCell>
-          <MdlCell cellLength="4">side pls</MdlCell>
+          <MdlCell cellLength="4">
+            <Link to="">
+              <button disabled={this.shouldButtonEnable()} className="mdl-button mdl-js-button mdl-button--raised
+              mdl-js-ripple-effect mdl-button--accent big-button">
+                Create questions
+              </button>
+            </Link>
+
+          </MdlCell>
         </OuterLayer>
       </div>
     );
